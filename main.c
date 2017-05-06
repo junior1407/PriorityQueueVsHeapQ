9b9908add93911fe35c9d5e442c80a33ded299eb
Valdir de Souza Junior <vsj@ic.ufal.br>
2017-05-06 16:47:27 -0300

    Conserto Código de Comparação

--- main.c
+++ main.c
@@ -4,30 +4,34 @@
 int main() {
     PQueue * pq = NewPQueue();
     Heap * heap = CreateHeap();
-    FILE * output = fopen("saida.csv","w");
+    FILE * output_heap = fopen("saidaheap.csv","w");
+    FILE * output_pqueue= fopen("saidapqueue.csv","w");
     FILE * file = fopen("C:\\Users\\Valdir Jr\\Documents\\GitHub\\PriorityQueueVsHeapQ\\numeros.txt","r");
-    int numero;
-    fprintf(output,"size,comparisons,algorithm\n");
-  /*  while( fscanf(file, "%d\n", &numero ) != EOF )
+    if (output_heap == NULL)
     {
+        printf("Erro ao abrir arquivo heap\n");
+    }
+    if (output_pqueue == NULL)
+    {
+        printf("Erro ao abrir arquivo Pqueue\n");
+    }
+    if (file == NULL)
+    {
+        printf("Erro ao abrir arquivo numeros\n");
+    }
+    int numero;
+    fprintf(output_heap,"size,comparisons\n");
+    fprintf(output_pqueue,"size,comparisons\n");
+    int contador=0;
+    while( fscanf(file, "%d\n", &numero ) != EOF )
+    {
+        fprintf(output_heap, "%d,%d\n", contador,HEnqueue(heap,numero));
+        fprintf(output_pqueue, "%d,%d\n", contador,Enqueue(pq,0,numero));
+        contador++;
+    }
 
-        //fprintf(output, "%d,%d,HeapQ\n", GetHeapSize(heap)-1,HEnqueue(heap,numero));
-        fprintf(output, "%d,%d,PQueue\n", GetPQueueSize(pq),Enqueue(pq,0,numero));
-
-    }*/
-
-
-    printf("%d,%d,PQueue\n", GetPQueueSize(pq),Enqueue(pq,1,1));
-    printf("%d,%d,PQueue\n", GetPQueueSize(pq),Enqueue(pq,2,4));
-    printf("%d,%d,PQueue\n", GetPQueueSize(pq),Enqueue(pq,3,3));
-    printf("%d\n",Dequeue(pq));
-    printf("%d\n",Dequeue(pq));
-    printf("%d\n",Dequeue(pq));
-    CleanPQueue(pq);
-    printf("%d\n",Enqueue(pq,100,50));
-    printf("%d\n",Enqueue(pq, 20,30));
-    printf("%d\n",Enqueue(pq, 0,40));
-//0,1,2
-
+    fclose(output_heap);
+    fclose(output_pqueue);
+    fclose(file);
     return 0;
 }
